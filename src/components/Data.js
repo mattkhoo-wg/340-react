d1b3ae748e3cdb782b4f21c7ebf71b5552b99a51
Matthew Khoo <mattkhoo1234@gmail.com>
2022-05-23 20:11:18 -0700

    fixed data page

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,26 +1,24 @@
 import React from 'react';
 import { NavBar } from './Navigation';
-
+import '../css/dataVis.css';
 
 function Filter(props) {
     return (
         <div className="filter">
             <h2>Filter Category</h2>
             <div className="filterInner">
-                <input type="checkbox" id="filter1" name="filter1" value="filter1"/>
-                <label for="filter1"> filter 1</label>
-                <input type="checkbox" id="filter2" name="filter2" value="filter2"/>
-                <label for="filter2"> filter 2</label>
-                <input type="checkbox" id="filter3" name="filter3" value="filter3"/>
-                <label for="filter3"> filter 3</label>
-            </div>
-            <div className="filterInner">
-                <input type="checkbox" id="filter1" name="filter1" value="filter1"/>
-                <label for="filter1"> filter 1</label>
-                <input type="checkbox" id="filter2" name="filter2" value="filter2"/>
-                <label for="filter2"> filter 2</label>
-                <input type="checkbox" id="filter3" name="filter3" value="filter3"/>
-                <label for="filter3"> filter 3</label>
+                <div>
+                    <input type="checkbox" id="filter1" name="filter1" value="filter1"/>
+                    <label for="filter1">Household Size</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="filter2" name="filter2" value="filter2"/>
+                    <label for="filter2">Travel</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="filter3" name="filter3" value="filter3"/>
+                    <label for="filter3">Diet</label>
+                </div>
             </div>
         </div>
     )
@@ -28,13 +26,16 @@ function Filter(props) {
 
 export function Data() {
     return (
-        <div>
+
+        <div class="container">
             <NavBar/>
-            <div className="container">
-                {/* {filters} */}
-                <div class="vis">
-                    <img src="img/emissions2.png" alt="image of co2 emissions by country"/>
+            <div class="filters">
+                <div className="container">
+                    <Filter/>
                 </div>
+            </div>
+            <div class="vis">
+                <img src="img/emissions2.png" alt="image of co2 emissions by country"/>
             </div>
         </div>
     )
