9e2599ea61297ea2deebb4d769f323d874a0e850
Jun Ahn <junahn45@gmail.com>
2022-05-24 23:13:03 -0700

    firebase

--- src/components/Result.js
+++ src/components/Result.js
@@ -5,8 +5,7 @@ export default function result(props){
     return (
     <div class="content">
         <div class="question-container">
-            <h1>Your answer to the previous question:</h1>
-            <h2>{props.surveyAnswer}</h2>
+            <h1>{"Your answer to the previous question: " + props.surveyAnswer}</h1>
         </div>
     </div>
     )
--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAARybFxshJw8bFKK9twBHZNklwf9MuJcU",
+  authDomain: "info340webapp-1e45c.firebaseapp.com",
+  projectId: "info340webapp-1e45c",
+  storageBucket: "info340webapp-1e45c.appspot.com",
+  messagingSenderId: "589323061955",
+  appId: "1:589323061955:web:4f57d41e7f86bb13107076"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
